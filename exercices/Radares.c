02e32c541ad89e0c6a64f1e002a803d8c47c5f19
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2025-04-02 22:29:27 -0300

    Criação da primeira parte da lógica do exercíci

--- exercices/Radares.c
+++ exercices/Radares.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+double calculaVelocidadeMedia(int tA,int tB, double distancia){
+    double converter_Segundos_Horas = (tB-tA) / 3600.0;
+    double kmPorHoras = (distancia) / (converter_Segundos_Horas);
+    return kmPorHoras;
+}
 
 int main()
 {
-
     
+    printf("%f", calculaVelocidadeMedia(61200,63000,60.0));    
+
+
     return 0;
 }
