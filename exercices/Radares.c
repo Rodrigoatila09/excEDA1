aabbf9a5adfdd5fcb292197f25a2dc8e339ea4a3
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2025-04-02 23:00:40 -0300

    Exercício Finalizado

--- exercices/Radares.c
+++ exercices/Radares.c
@@ -6,11 +6,13 @@ double calculaVelocidadeMedia(int tA,int tB, double distancia){
     return kmPorHoras;
 }
 
-int main()
-{
-    
-    printf("%f", calculaVelocidadeMedia(61200,63000,60.0));    
-
-
-    return 0;
+int levouMulta(int tA, int tB, double distancia, double velocidadeMaxima){
+    if (calculaVelocidadeMedia(tA,tB,distancia) > velocidadeMaxima)
+    {
+        return 1;
+    }else{
+        return 0;
+    }
 }
+
+
