dacb533996806a989bb6b3d340621cea59c7513f
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2025-04-04 09:26:28 -0300

    Exercicio Finalizado!

--- exercices/MaiorqueMedia.c
+++ exercices/MaiorqueMedia.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 
-int main()
-{
-
-
+int main() {   
+    int tamanhoArray;
+    scanf("%d", &tamanhoArray);
+    int arrayNumeros[tamanhoArray];
+    long long soma = 0;
     
+    for(int i = 0; i < tamanhoArray; i++){ 
+        scanf("%d", &arrayNumeros[i]);
+        soma += arrayNumeros[i];
+    }
+    
+    int media = (int)(soma / tamanhoArray);
+    int primeiro = 1;  
+
+    for (int j = 0; j < tamanhoArray; j++) {
+        if(arrayNumeros[j] > media) {
+            if(!primeiro) printf(" "); 
+            printf("%d", arrayNumeros[j]);
+            primeiro = 0;
+        }
+    }
+
+    if(primeiro) {  
+        printf("0");
+    }
+    
+    printf("\n");  
     return 0;
 }
